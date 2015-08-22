1d6e2b1ea3686ff4eec1489a574bb19ce039635c
Mnenmenth <iamrazim12@gmail.com>
2015-08-22 00:00:13 -0400

    Woops sorry torri

--- src/main/scala/ai/Synapse.scala
+++ src/main/scala/ai/Synapse.scala
@@ -4,7 +4,7 @@ package ai
  * Connection to dendrites
  * Created by Torri on 8/21/2015.
  */
-/*class Synapse(val from:Neuron, val to:Neuron, val weight:Double) {
+class Synapse(val from:Neuron, val to:Neuron, val weight:Double) {
   var active = false
   def activate = {
     active = true
@@ -13,4 +13,3 @@ package ai
     active = false
   }
 }
-*/
